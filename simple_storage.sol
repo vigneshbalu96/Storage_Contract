162882f6be1df63ee3ef59c85a19a890af9bd8d6
Vignesh Blausamy <vigneshbalu96@gmail.com>
2024-06-22 12:59:36 +0530

    Second commit

--- simple_storage.sol
+++ simple_storage.sol
@@ -1,41 +1,31 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.25;
+pragma solidity ^0.8.24;
 
 contract SimpleStorage {
-    // ; says that the statement has completed!
-    uint256  HasFavouriteNumber;
-    
-    //uint256[] public listOfFavoutireNumbers; //Dynemic Array
 
+    uint256  hasFavouriteNumber;
+    
+    // custom Datatype is used to store the people's Data
     struct person {
         uint256 favouriteNumber;
         string name;
     }
-    // person public nairobi = person(5, "nairobi"); // Good for 1 friend
-    //person public tokyo = person({favouriteNumber: 5, name: "tokyo"}); //Being Explicit here //for many friends do I need to copy paste this line many as many frds I have == no createae an array and push
+    
     person[] public friendsList;
 
+    // mapping datatype helps to get the people's data with respect to corresponding member.
     mapping(string => uint256) public nameToFavouriteNumber;
 
     function addFriends(uint256 _favouriteNumber, string memory _name) public {
-        // person memory newFriend = person(_favouriteNumber, _name);
-        // friendsList.push(newFriend);
-        //or
         friendsList.push(person(_favouriteNumber, _name));
         nameToFavouriteNumber[_name] = _favouriteNumber;
     }
 
     function store(uint256 _favouriteNumber) public virtual {
-        HasFavouriteNumber=_favouriteNumber;
-        //retrieve();  // This retrive cost Gas
+        hasFavouriteNumber=_favouriteNumber;
     }
+
     function retrieve() public view returns(uint256) {
-        return HasFavouriteNumber;
+        return hasFavouriteNumber;
     }   
 }
-
-
-contract SimpleStorage2 {}
-contract SimpleStorage3 {}
-contract SimpleStorage4 {}
-contract SimpleStorage5 {}
